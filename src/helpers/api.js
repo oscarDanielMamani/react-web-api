38205c4e538a2e5d1633424b533c427c7f943beb
daniel mamani <dani.oscar.257@gmail.com>
2021-10-30 11:37:15 -0300

    OK: archivo con funciones para la webApi

---
+++ src/helpers/api.js
@@ -0,0 +1,53 @@
+//TODO: en un app real cargariamos esto de un archivo de configuracion o de variables de entorno
+var BACKEND_URL= "http://localhost:3000";
+//A: base url
+var NOTES_URL= BACKEND_URL + "/notes";
+//A: la url para obtener la lista de notas de nuestra web Api
+
+// one_note_url= notes_url + "/" + id;
+
+async function notasListar(){
+  var myHeaders = new Headers();
+  myHeaders.append("Accept", "application/json");
+  myHeaders.append("Content-Type", "application/json");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  
+  let result= await fetch(NOTES_URL, requestOptions);
+  let resultJson= await result.json();
+  return resultJson;
+}
+
+async function notaBorrar(id){
+  var myHeaders = new Headers();
+  myHeaders.append("Accept", "application/json");
+  myHeaders.append("Content-Type", "application/json");
+
+  var requestOptions = {
+    method: 'DELETE',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  let urlBorrado= `${NOTES_URL}/${id}`;
+  
+  let result= await fetch(urlBorrado, requestOptions);
+  let resultJson= await result.json();
+  return resultJson;
+}
+
+module.exports={
+  notasListar,
+  notaBorrar
+}
+
+// notasListar
+// notaEditar
+// notaBorrar
+// notaCrear
+//las funciones que esperamos tener para interactuar con la api
